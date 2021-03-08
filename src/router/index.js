872c15519d8e646521ca6e6f57b0d5ebf5bedca0
filipmagula <filip.magula@gmail.com>
2021-03-08 13:26:23 +0100

    baseinput template for form

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import EventList from '../views/EventList.vue'
 import About from '../views/About.vue'
 import EventDetails from '../views/EventDetails.vue'
+import NewEvent from '../views/NewEvent.vue'
 
 const routes = [
   {
@@ -15,6 +16,12 @@ const routes = [
     props: true,
     
     component: EventDetails
+  },
+  {
+    path: '/event/new',
+    name: 'NewEvent',
+    
+    component: NewEvent
   },
   {
     path: '/about',
